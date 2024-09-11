9325243f63306c3a756ee3e110971e0f4daa1d7a
Luke Adams <lradams@unc.edu>
2024-09-11 12:18:16 -0400

    more work on conditionals

--- lessons/conditionals.py
+++ lessons/conditionals.py
@@ -1,15 +1,19 @@
 """Practice with conditionals."""
 
 
-def less_than_10(num: int) -> bool:
+def less_than_10(num: int) -> None:
     """Tell me if num is < 10."""
+    dub: int = num * 2
+    dub = dub - 1
+    print(dub)
     if num < 10:
         print("Small number!")
     else:
         print("Big number!")
+    print("Wow!")
 
 
-# less_than_10(num=8)
+# less_than_10(num=5)
 
 
 def should_i_eat(hungry: bool) -> None:
@@ -31,5 +35,20 @@ def check_first_letter(word: str, letter: str) -> str:
         return "No match."
 
 
-print(check_first_letter(word="happy", letter="h"))
-print(check_first_letter(word="happy", letter="s"))
+# print(check_first_letter(word="happy", letter="h"))
+# print(check_first_letter(word="happy", letter="s"))
+
+
+def get_weather_report() -> str:
+    """Display weather advice."""
+    weather: str = input("What is the weather?")
+    if weather == "rainy" or weather == "cold":
+        print("Bring a jacket!")
+    elif weather == "hot":
+        print("Keep cool out there!")
+    else:
+        print("I don't recognize this weather.")
+    return weather
+
+
+get_weather_report()
