10ab4fc794b87cd95755467368e279286bb74891
Luke Adams <lradams@unc.edu>
2024-09-25 16:53:03 -0400

    Final surge in ex02

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -7,6 +7,7 @@ def input_word() -> str:
     word: str = input("Enter a 5-character word: ")
     if 5 != len(word):
         print("Error: Word must contain 5 characters.")
+    exit()  # exit() cuts the function off here
     return word
 
 
@@ -14,14 +15,42 @@ def input_letter() -> str:
     letter: str = input("Enter a single character: ")
     if not 1 == len(letter):
         print("Error: Character must be a single character.")
+    exit()  # exit() cuts the function off here
     return letter
 
 
 def contains_char(word: str, letter: str) -> None:
-    if word == input_word and letter == input_letter:
-        
+    count: int = 0  # the word starts with 0 characters known
     print("Searching for " + letter + " in " + word)
+    if letter == word[0]:
+        print(letter + " found at index 0")
+        count += 1
+    if letter == word[1]:
+        print(letter + " found at index 1")
+        count += 1
+    if letter == word[2]:
+        print(letter + " found at index 2")
+        count += 1
+    if letter == word[3]:
+        print(letter + " found at index 3")
+        count += 1
+    if letter == word[4]:
+        print(letter + " found at index 4")
+        count += 1
+    if count == 0:
+        print("No instances of " + letter + " found in " + word)
+    elif count == 1:
+        print("1 instance of " + letter + " found in " + word)
+    else:
+        print(
+            str(count) + " instances of " + letter + " found in " + word
+        )  # count is int but must be str
+
+
+def main() -> None:
+    word: str = input_word
+    letter: str = input_letter
 
 
 # how to import code for use in the REPL:
-# from exercises.ex02_chardle import input_function
+# from exercises.ex02_chardle import function_name
