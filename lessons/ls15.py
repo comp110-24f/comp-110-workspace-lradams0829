33accef6a65ba235f7db20aa7f1a7665ab79f9de
Luke Adams <lradams@unc.edu>
2024-10-07 12:05:21 -0400

    ls15 stuff

---
+++ lessons/ls15.py
@@ -0,0 +1,7 @@
+pets: list[str] = ["Louie", "Bo", "Bear"]
+for good_boy in pets:
+    print(f"Good boy, {good_boy}!")
+
+names: list[str] = ["Alyssa", "Janet", "Vrinda"]
+for idx_name in range(0, len(names)):
+    print(f"{idx_name}: {names[idx_name]}")
