62e1e5d3ed136a2a8526fabef2e4f53d02078b59
Luke Adams <lradams@unc.edu>
2024-10-12 13:05:31 -0400

    ex04 is done (I think)

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -16,6 +16,11 @@ def all(vals: list[int], val: int) -> bool:
     return True
 
 
+# since using while loop, variable called index is necessary
+# include "or vals == []" so that while loop is entered if vals == []
+# instead of recommendation in line 20, the if statement could be moved outside of loop
+
+
 def max(vals: list[int]) -> int:
     """find and return the largest int in the list"""
     if len(vals) == 0:
@@ -31,5 +36,31 @@ def max(vals: list[int]) -> int:
     return max_val
 
 
+# since using while loop, variable called index is necessary
+# max_val variable is helpful to keep track of the largest number
+# else block not completely necessary, can move "index += 1" into if block
+
+
 def is_equal(list_1: list[int], list_2: list[int]) -> bool:
     """determine whether or not 2 lists are deep equals"""
+    if len(list_1) != len(list_2):
+        return False
+    for index in range(0, len(list_1)):
+        if list_1[index] != list_2[index]:
+            return False
+    return True
+
+
+# use of for loop with range makes the code much shorter
+# implement code that returns False right away if the lists aren't of equal length
+# due to lines 36-37, range of only a single list is necessary
+
+
+def extend(list_1: list[int], list_2: list[int]) -> None:
+    """append the second list to the end of the first list"""
+    for elem in list_2:
+        list_1.append(elem)
+
+
+# using for loop scans across every elem in list_2
+# the loop finishes once the last elem of list_2 is added to list_1
